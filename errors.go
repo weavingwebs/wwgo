28c35f45be20ba79d7ebd4bb79adbcb723b27be2
chris <chris@weavingwebs.co.uk>
2021-08-09 11:21:29 +0100

    Add ClientError & obfuscate all other errors from GQL responses by default
    Closes #2

--- errors.go
+++ errors.go
@@ -3,7 +3,29 @@ package wwgo
 import (
 	"fmt"
 	"github.com/pkg/errors"
+	"runtime"
 )
+
+// stack is a direct copy from pkg/errors.
+type stack []uintptr
+
+// StackTrace is a direct copy from pkg/errors.
+func (s *stack) StackTrace() errors.StackTrace {
+	f := make([]errors.Frame, len(*s))
+	for i := 0; i < len(f); i++ {
+		f[i] = errors.Frame((*s)[i])
+	}
+	return f
+}
+
+// callers is based on pkg/errors.
+func callers() *stack {
+	const depth = 8
+	var pcs [depth]uintptr
+	n := runtime.Callers(3, pcs[:])
+	var st stack = pcs[0:n]
+	return &st
+}
 
 type stackTracer interface {
 	StackTrace() errors.StackTrace
@@ -13,5 +35,38 @@ func PrintErrWithStack(err error) {
 	fmt.Printf("Error: %s\n", err)
 	if stackErr, ok := err.(stackTracer); ok {
 		fmt.Println(stackErr.StackTrace())
+	}
+}
+
+// ClientError represents an error that should be displayed to the user.
+type ClientError struct {
+	message       string
+	code          string
+	originalError error
+	stack         *stack
+}
+
+func (err *ClientError) GqlErrorCode() string {
+	return err.code
+}
+
+func (err *ClientError) Error() string {
+	return err.message
+}
+
+func (err *ClientError) Unwrap() error {
+	return err.originalError
+}
+
+func (err *ClientError) StackTrace() errors.StackTrace {
+	return err.stack.StackTrace()
+}
+
+func NewClientError(code string, message string, err error) *ClientError {
+	return &ClientError{
+		message:       message,
+		code:          code,
+		originalError: err,
+		stack:         callers(),
 	}
 }
--- wwgraphql/graphql.go
+++ wwgraphql/graphql.go
@@ -2,11 +2,15 @@ package wwgraphql
 
 import (
 	"context"
+	"fmt"
 	"github.com/99designs/gqlgen/graphql"
+	"github.com/99designs/gqlgen/graphql/errcode"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/extension"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+	"github.com/vektah/gqlparser/v2/gqlerror"
 	"github.com/weavingwebs/wwgo"
 	"regexp"
 	"strings"
@@ -18,7 +22,7 @@ var MB int64 = 1 << 20
 var cognitoEmailRegexp = regexp.MustCompile(`[\p{L}\p{M}\p{S}\p{N}\p{P}]+`)
 var emailRegexp = regexp.MustCompile(`^[^\s]+@[^\s]+\.[^\s]+$`)
 
-func NewGraphQlServer(es graphql.ExecutableSchema) *handler.Server {
+func NewGraphQlServer(es graphql.ExecutableSchema, log zerolog.Logger) *handler.Server {
 	srv := handler.New(es)
 
 	srv.AddTransport(transport.Websocket{
@@ -34,7 +38,50 @@ func NewGraphQlServer(es graphql.ExecutableSchema) *handler.Server {
 
 	srv.Use(extension.Introspection{})
 
+	srv.SetErrorPresenter(DefaultErrorPresenter(log))
+
 	return srv
+}
+
+func DefaultErrorPresenter(log zerolog.Logger) func(ctx context.Context, e error) *gqlerror.Error {
+	return func(ctx context.Context, e error) *gqlerror.Error {
+		// Always log all errors, if it's wrapped by GQL then log the original.
+		logEvt := log.Error().Stack()
+		gqlErr, ok := e.(*gqlerror.Error)
+		var originalErr error
+		if ok {
+			originalErr = gqlErr.Unwrap()
+		}
+		if originalErr != nil {
+			logEvt.Err(originalErr).Str("gqlPath", gqlErr.Path.String())
+		} else {
+			logEvt.Err(e)
+		}
+		logEvt.Send()
+
+		// Create the GQL response
+		errResp := graphql.DefaultErrorPresenter(ctx, e)
+		if errResp.Extensions == nil {
+			errResp.Extensions = map[string]interface{}{}
+		}
+
+		// Check if it is a client error.
+		var clientErr *wwgo.ClientError
+		if errors.As(e, &clientErr) {
+			if clientErr.GqlErrorCode() != "" {
+				errResp.Extensions["code"] = clientErr.GqlErrorCode()
+			}
+			// Use the message from the client error directly, it may have been
+			// wrapped by another error that is not client safe.
+			errResp.Message = clientErr.Error()
+		} else if errResp.Extensions["code"] != errcode.ValidationFailed && errResp.Extensions["code"] != errcode.ParseFailed {
+			// If the error is not a ClientError or GQL validation, obfuscate it.
+			errResp.Message = "An unexpected error occurred, please try again later"
+			errResp.Extensions["code"] = 500
+		}
+
+		return errResp
+	}
 }
 
 // @todo figure out how to put validation directives into a gqlgen plugin?
@@ -77,22 +124,34 @@ func ValidateStringDirective(ctx context.Context, obj interface{}, next graphql.
 
 	// Validate.
 	if rules.MinLength != nil && len(str) < *rules.MinLength {
-		// @todo gql error code.
-		return nil, errors.Errorf("Cannot be less than %d %s", *rules.MinLength, wwgo.Plural(*rules.MinLength, "character", "characters"))
+		return nil, wwgo.NewClientError(
+			"VALIDATE_STRING_MIN_LENGTH_EXCEPTION",
+			fmt.Sprintf("Cannot be less than %d %s", *rules.MinLength, wwgo.Plural(*rules.MinLength, "character", "characters")),
+			nil,
+		)
 	}
 	if rules.MaxLength != nil && len(str) > *rules.MaxLength {
-		// @todo gql error code.
-		return nil, errors.Errorf("Cannot be more than %d %s", *rules.MaxLength, wwgo.Plural(*rules.MaxLength, "character", "characters"))
+		return nil, wwgo.NewClientError(
+			"VALIDATE_STRING_MAX_LENGTH_EXCEPTION",
+			fmt.Sprintf("Cannot be more than %d %s", *rules.MaxLength, wwgo.Plural(*rules.MaxLength, "character", "characters")),
+			nil,
+		)
 	}
 	if rules.Pattern != nil {
 		switch strings.ToUpper(*rules.Pattern) {
 		case "EMAIL":
 			if !cognitoEmailRegexp.MatchString(str) {
-				// @todo gql error code.
-				return nil, errors.New("Email contains invalid characters")
+				return nil, wwgo.NewClientError(
+					"VALIDATE_STRING_PATTERN_EMAIL_CHARS_EXCEPTION",
+					"Email contains invalid characters",
+					nil,
+				)
 			} else if !emailRegexp.MatchString(str) {
-				// @todo gql error code.
-				return nil, errors.New("Please enter a valid email address")
+				return nil, wwgo.NewClientError(
+					"VALIDATE_STRING_PATTERN_EMAIL_FORMAT_EXCEPTION",
+					"Please enter a valid email address",
+					nil,
+				)
 			}
 		}
 	}
