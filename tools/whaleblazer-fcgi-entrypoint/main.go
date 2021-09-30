e32146bf322ee97f40ac5df304e1d1f90dc735e6
chris <chris@weavingwebs.co.uk>
2021-09-30 16:42:44 +0100

    Proxy OS signals to command in whaleblazer-fcgi-entrypoint

--- tools/whaleblazer-fcgi-entrypoint/main.go
+++ tools/whaleblazer-fcgi-entrypoint/main.go
@@ -5,6 +5,7 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 	"os"
 	"os/exec"
+	"os/signal"
 	"path"
 	"syscall"
 )
@@ -32,22 +33,54 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Create command.
 	cmd := exec.Command(args[1], args[2:]...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Credential: &syscall.Credential{
 			Uid: 48,
 			Gid: 48,
 		},
-		Setpgid: true,
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
-		if exitErr, ok := err.(*exec.ExitError); ok {
-			os.Exit(exitErr.ExitCode())
-		}
-		_, _ = fmt.Fprintf(os.Stderr, "Failed to run cmd: %s\n", err)
+
+	// Start command.
+	if err := cmd.Start(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Failed to start cmd: %s\n", err)
 		os.Exit(1)
+	}
+
+	// Create a channel for the command.
+	waitCh := make(chan error, 1)
+	go func() {
+		waitCh <- cmd.Wait()
+		close(waitCh)
+	}()
+
+	// Create a channel for OS signals.
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan)
+
+	// Wait for command & proxy signals.
+	for {
+		select {
+
+		case sig := <-sigChan:
+			_ = cmd.Process.Signal(sig)
+
+		case err := <-waitCh:
+			var waitStatus syscall.WaitStatus
+			if exitError, ok := err.(*exec.ExitError); ok {
+				waitStatus = exitError.Sys().(syscall.WaitStatus)
+				os.Exit(waitStatus.ExitStatus())
+			}
+			if err != nil {
+				_, _ = fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			return
+
+		}
 	}
 }
