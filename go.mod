74ed8dada29e944ae7e2747875406d2f20209b4d
chris <chris@weavingwebs.co.uk>
2023-07-19 16:49:18 +0100

    Upgrade jwt to v5 and rework accordingly

--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/go-chi/cors v1.2.1
 	github.com/go-chi/httplog v0.3.0
 	github.com/go-sql-driver/mysql v1.7.1
-	github.com/golang-jwt/jwt/v4 v4.5.0
+	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/golang-migrate/migrate/v4 v4.15.2
 	github.com/google/uuid v1.3.0
 	github.com/jmoiron/sqlx v1.3.5
--- wwauth/jwt.go
+++ wwauth/jwt.go
@@ -2,10 +2,11 @@ package wwauth
 
 import (
 	"context"
-	"github.com/golang-jwt/jwt/v4"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/lestrrat-go/jwx/jwk"
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"net/http"
 	"strings"
 	"time"
@@ -17,88 +18,70 @@ type contextKey struct {
 	name string
 }
 
-type Claims interface {
-	Valid() error
-	VerifyIssuer() bool
-	VerifyAudience() bool
-	GetStandardClaims() jwt.RegisteredClaims
+type JwtAuthOpt struct {
+	Jwks     jwk.Set
+	Issuer   string
+	Audience string
+	// DANGER: It is very important for newClaims to return a fresh claims pointer,
+	// otherwise all requests will share the same JWT claims pointer!
+	NewClaims func() jwt.Claims
 }
 
-// JwtMiddleware
-//
+type JwtAuth struct {
+	log zerolog.Logger
+	JwtAuthOpt
+}
+
+// NewJwtAuth
 // DANGER: It is very important for newClaims to return a fresh claims pointer,
 // otherwise all requests will share the same JWT claims pointer!
-func JwtMiddleware(
-	jwks jwk.Set,
-	log zerolog.Logger,
-	newClaims func() Claims,
-) func(http.Handler) http.Handler {
-	// Manually allow for 10s clock drift to avoid IAT validation errors.
-	// @todo Remove this once IAT validation has been removed.
-	//   https://github.com/golang-jwt/jwt/issues/98
-	jwt.TimeFunc = func() time.Time {
-		return time.Now().UTC().Add(10 * time.Second)
-	}
-
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Check for token.
-			tokenStr := TokenFromHeader(r)
-			if tokenStr == "" {
-				log.Trace().Msg("No JWT found")
-				next.ServeHTTP(w, r)
-				return
-			}
-
-			// Parse the token.
-			token, err := ParseJwt(jwks, tokenStr, newClaims)
-			if err != nil {
-				if vErr, ok := err.(*jwt.ValidationError); ok {
-					if vErr.Errors&jwt.ValidationErrorExpired > 0 {
-						http.Error(w, "JWT has expired", 401)
-					} else if vErr.Errors&jwt.ValidationErrorIssuedAt > 0 {
-						http.Error(w, "JWT IAT validation failed", 401)
-					} else if vErr.Errors&jwt.ValidationErrorNotValidYet > 0 {
-						http.Error(w, "JWT is not valid yet", 401)
-					} else if vErr.Errors&jwt.ValidationErrorNotValidYet > 0 {
-					} else {
-						http.Error(w, "JWT validation error", 401)
-					}
-
-					log.Error().Err(vErr).Stack().Msg("JWT validation error")
-					return
-				} else if errors.Is(err, jwt.ErrTokenInvalidIssuer) {
-					http.Error(w, "Invalid JWT issuer", 401)
-				} else if errors.Is(err, jwt.ErrTokenInvalidAudience) {
-					http.Error(w, "Invalid JWT audience", 401)
-				} else if errors.Is(err, jwt.ErrTokenInvalidClaims) {
-					http.Error(w, "Invalid JWT claims", 401)
-				} else {
-					log.Error().Err(err).Stack().Msg("Generic JWT error")
-				}
-				http.Error(w, "JWT error", 500)
-				return
-			}
-
-			// Add to context.
-			next.ServeHTTP(w, r.WithContext(ContextWithJwt(r.Context(), token)))
-		})
+func NewJwtAuth(log zerolog.Logger, opt JwtAuthOpt) *JwtAuth {
+	return &JwtAuth{
+		log:        log,
+		JwtAuthOpt: opt,
 	}
 }
 
-// ParseJwt
-//
-// DANGER: It is very important for newClaims to return a fresh claims pointer,
-// otherwise all requests will share the same JWT claims pointer!
-func ParseJwt(jwks jwk.Set, tokenStr string, newClaims func() Claims) (*jwt.Token, error) {
+func (auth *JwtAuth) JwtMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Check for token.
+		tokenStr := TokenFromHeader(r)
+		if tokenStr == "" {
+			log.Trace().Msg("No JWT found")
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		// Parse the token.
+		token, err := auth.ParseJwt(tokenStr)
+		if err != nil {
+			log.Error().Err(err).Stack().Msg("JWT error")
+			if errors.Is(err, jwt.ErrTokenExpired) {
+				http.Error(w, "JWT has expired", http.StatusUnauthorized)
+			}
+			http.Error(w, "JWT error", http.StatusUnauthorized)
+			return
+		}
+
+		// Add to context.
+		next.ServeHTTP(w, r.WithContext(ContextWithJwt(r.Context(), token)))
+	})
+}
+
+func (auth *JwtAuth) ParseJwt(tokenStr string) (*jwt.Token, error) {
 	// Parse the token.
-	claims := newClaims()
-	token, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+	claims := auth.NewClaims()
+	parser := jwt.NewParser(
+		jwt.WithIssuer(auth.Issuer),
+		jwt.WithAudience(auth.Audience),
+		jwt.WithLeeway(10*time.Second),
+	)
+	token, err := parser.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
 		kid, ok := token.Header["kid"].(string)
 		if !ok {
 			return nil, errors.New("kid header not found in jwt")
 		}
-		key, ok := jwks.LookupKeyID(kid)
+		key, ok := auth.Jwks.LookupKeyID(kid)
 		if !ok {
 			return nil, errors.Errorf("key %v not found in jwks", kid)
 		}
@@ -116,18 +99,6 @@ func ParseJwt(jwks jwk.Set, tokenStr string, newClaims func() Claims) (*jwt.Toke
 	// Check the validity of the token.
 	if !token.Valid {
 		return nil, errors.Errorf("Invalid token")
-	}
-
-	// Verify issuer.
-	if tokenClaims, ok := token.Claims.(Claims); ok {
-		if !tokenClaims.VerifyIssuer() {
-			return nil, errors.Wrapf(jwt.ErrTokenInvalidIssuer, "Invalid JWT issuer: %s", tokenClaims.GetStandardClaims().Issuer)
-		}
-		if !tokenClaims.VerifyAudience() {
-			return nil, errors.Wrapf(jwt.ErrTokenInvalidAudience, "Invalid JWT audience: %s", tokenClaims.GetStandardClaims().Audience)
-		}
-	} else {
-		return nil, errors.Wrapf(jwt.ErrTokenInvalidClaims, "unknown type of Claims in jwt: %T", token.Claims)
 	}
 
 	return token, nil
