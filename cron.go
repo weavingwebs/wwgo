e017352115fedde5f24d2805230c0d927a15d08e
chris <chris@weavingwebs.co.uk>
2023-12-18 11:17:37 +0000

    Add cron helper

---
+++ cron.go
@@ -0,0 +1,96 @@
+package wwgo
+
+import (
+	"context"
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/robfig/cron/v3"
+	"github.com/rs/zerolog"
+	"time"
+)
+
+type CronTab struct {
+	log      zerolog.Logger
+	slack    *SlackWebhookClient
+	siteName string
+	crons    map[string]func(ctx context.Context, log zerolog.Logger) error
+	timeZone *time.Location
+}
+
+func NewCronTab(
+	log zerolog.Logger,
+	slack *SlackWebhookClient,
+	siteName string,
+	// i.e. "Europe/London", "UTC" or "Local" (not recommended).
+	// See https://golang.org/pkg/time/#LoadLocation for more info.
+	timeZoneName string,
+) (*CronTab, error) {
+	var timeZone *time.Location
+	var err error
+	if timeZone, err = time.LoadLocation(timeZoneName); err != nil {
+		return nil, errors.Wrapf(err, "failed to load timezone '%s'", timeZoneName)
+	}
+
+	return &CronTab{
+		log:      log,
+		slack:    slack,
+		siteName: siteName,
+		crons:    map[string]func(ctx context.Context, log zerolog.Logger) error{},
+		timeZone: timeZone,
+	}, nil
+}
+
+// Start the crons in the background.
+func (c *CronTab) Start(ctx context.Context) {
+	log := c.log
+
+	crons := cron.New(
+		cron.WithLocation(c.timeZone),
+		cron.WithLogger(zerologCronLogger{c.log}),
+	)
+
+	// Add crons.
+	for spec, fn := range c.crons {
+		if _, err := crons.AddFunc(spec, func() {
+			if err := fn(ctx, log); err != nil {
+				log.Err(err).Send()
+				c.slack.Send(ctx, SlackMessagePayload{
+					Channel:   nil,
+					Username:  ToPtr(c.siteName),
+					Text:      fmt.Sprintf("Cron job failed with error: %s", err),
+					IconEmoji: ToPtr(":face_with_symbols_on_mouth:"),
+				})
+			}
+		}); err != nil {
+			log.Fatal().Err(err).Msgf("Failed to add cron %s", spec)
+		}
+	}
+
+	// Run crons in background.
+	crons.Start()
+
+	// Stop crons on context cancel.
+	go func() {
+		<-ctx.Done()
+		crons.Stop()
+	}()
+}
+
+type zerologCronLogger struct {
+	log zerolog.Logger
+}
+
+func (l zerologCronLogger) Info(msg string, keysAndValues ...interface{}) {
+	zerologCronLog(l.log.Info(), msg, keysAndValues)
+}
+
+func (l zerologCronLogger) Error(err error, msg string, keysAndValues ...interface{}) {
+	zerologCronLog(l.log.Err(err), msg, keysAndValues)
+}
+
+func zerologCronLog(logEntry *zerolog.Event, msg string, keysAndValues ...interface{}) {
+	if len(keysAndValues) != 0 {
+		logEntry.Interface("cronContext", keysAndValues)
+	}
+	logEntry.Msgf(msg)
+}
--- go.mod
+++ go.mod
@@ -63,6 +63,7 @@ require (
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
